59631ac92f2c84ea6e93840ce4e99935e2fb768b
RishiiiiPatel06 <codewithrishi01.in>
2025-09-26 13:06:30 +0530

    Update db.py

--- db.py
+++ db.py
@@ -9,7 +9,8 @@ def get_connection():
         database=st.secrets["DB_NAME"],
         user=st.secrets["DB_USER"],
         password=st.secrets["DB_PASS"],
-        port=st.secrets["DB_PORT"]
+        port=st.secrets["DB_PORT"],
+        sslmode="require"
     )
     return conn
 
